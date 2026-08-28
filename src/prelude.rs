b332f074bbee124d4ca2b18b4de8b5c496d993de
SpacingBat3 <spacingbat3@gmail.com>
2026-08-28 12:24:25 +0200

    chore: update `prelude`

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,4 +2,16 @@
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
+//! The Kontur prelude
+//! ====================================================================
+//! 
+//! This defines a set of traits and macros you might need to import to
+//! utilize all `kontur` features.
+//! 
+
+// Traits
 pub use crate::fmt::TtyFormat;
+pub use crate::terminal::{IsTerminal,PlatformTerminal};
+pub use crate::hook::KonturHook;
+// Macros
+pub use crate::macros::style::*;
