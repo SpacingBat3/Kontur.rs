cfdbcc4b5dd60be575d68981ec28447ee5255629
SpacingBat3 <spacingbat3@gmail.com>
2026-08-28 21:07:22 +0200

    fix(licenses): add `GPL-3.0-or-later`
    As per REUSE recomendations, LGPL-3.0 should be paired even if REUSE
    finds it unused, and license SPDX identifier should be LGPL-3.0-or-later
    OR GPL-3.0-or-later, given LGPL3 is additerium license to GPL3.

--- src/sys/windows/terminal.rs
+++ src/sys/windows/terminal.rs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2026 Dawid Papiewski "SpacingBat3" <spacingbat3@gmail.com>
 // SPDX-FileCopyrightText: 2026, 2026 Dawid Papiewski "SpacingBat3" <spacingbat3@gmail.com>
 //
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later OR GPL-3.0-or-later
 
 //! Platform-agnostic terminal manipulation APIs (Windows)
 
